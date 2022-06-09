162d88cb5b92577314ebaadefff797537799981a
Patrick <pat.manser@gmail.com>
2022-06-09 15:24:14 +0200

    add optional slot to content header

--- app/frontend/src/components/ContentHeader.svelte
+++ app/frontend/src/components/ContentHeader.svelte
@@ -4,9 +4,15 @@
 
     export let title: string
     export let breadcrumbs: Array<Breadcrumb>|null
+    export let useSlot: boolean = false;
 </script>
 
-<h1 class="text-wyrk-200 text-xl font-bold {breadcrumbs ? '' : 'mb-8'}">{title}</h1>
+<div class="flex justify-between content-center {breadcrumbs ? '' : 'mb-8'}">
+    <h1 class="text-wyrk-200 text-xl font-bold">{title}</h1>
+    {#if useSlot}
+        <slot></slot>
+    {/if}
+</div>
 
 {#if breadcrumbs}
     <Breadcrumbs items="{breadcrumbs}" />
